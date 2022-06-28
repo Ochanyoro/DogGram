4bf5c5f0e6dc20d9b6b3a6bece0f5514217d29ee
Ochanyoro <wadaoo0223@icloud.com>
2022-06-28 12:25:02 +0900

    Initial Commit

---
+++ DogGram/DogGramApp.swift
@@ -0,0 +1,17 @@
+//
+//  DogGramApp.swift
+//  DogGram
+//
+//  Created by 大和田一裕 on 2022/06/28.
+//
+
+import SwiftUI
+
+@main
+struct DogGramApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
