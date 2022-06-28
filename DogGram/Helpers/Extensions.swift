0e70c28bd59153dc7aff0a041b7ddcaf3d9f4321
Ochanyoro <wadaoo0223@icloud.com>
2022-06-28 14:24:03 +0900

    [create] TabView(ContentView), Extensions which added Color(Helpers)

---
+++ DogGram/Helpers/Extensions.swift
@@ -0,0 +1,31 @@
+//
+//  Extensions.swift
+//  DogGram
+//
+//  Created by 大和田一裕 on 2022/06/28.
+//
+
+import Foundation
+import SwiftUI
+
+
+extension Color {
+    
+    struct MyTheme {
+        static var purpleColor: Color {
+            return Color("ColorPurple")
+        }
+        
+        static var yellowColor: Color {
+            return Color("ColorYellow")
+        }
+        
+        static var beigeColor: Color {
+            return Color("ColorBeige")
+        }
+        
+        
+        
+    }
+    
+}
---
+++ DogGram/Views/Screens/ContentView.swift
@@ -0,0 +1,44 @@
+//
+//  ContentView.swift
+//  DogGram
+//
+//  Created by 大和田一裕 on 2022/06/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        TabView{
+            Text("Screen1")
+                .tabItem{
+                    Image(systemName: "book.fill")
+                    Text("Feed")
+                }
+            
+            Text("Screen2")
+                .tabItem{
+                    Image(systemName: "magnifyingglass")
+                    Text("Browse")
+                }
+            Text("Screen3")
+                .tabItem{
+                    Image(systemName: "square.amd.arrow.up.fill")
+                    Text("Upload")
+                }
+            
+            Text("Screen4")
+                .tabItem{
+                    Image(systemName: "person.fill")
+                    Text("Profile")
+                }
+        }
+        .accentColor(Color.MyTheme.purpleColor)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
