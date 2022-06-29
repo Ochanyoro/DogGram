93c97b4c701c8a86378d3013a2a0e995b684741e
Ochanyoro <wadaoo0223@icloud.com>
2022-06-29 20:32:45 +0900

    [create] OnboardingView(Screens) and SignInWithAppleButtonCustom(Subviews)

---
+++ DogGram/Views/Screens/OnboardingView.swift
@@ -0,0 +1,66 @@
+//
+//  OnboardingView.swift
+//  DogGram
+//
+//  Created by 大和田一裕 on 2022/06/29.
+//
+
+import SwiftUI
+
+struct OnboardingView: View {
+    var body: some View {
+        VStack(spacing: 10){
+            Image("logo.transparent")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 100, height: 100, alignment: .center)
+                .shadow(radius: 12)
+            
+            Text("Welcome to DogGram!")
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .foregroundColor(Color.MyTheme.purpleColor)
+            
+            Text("DogGram is the #1 app for posting pictures of your dog and sharing them across the world. We are a dog-loving community and we're happy to have you!")
+                .font(.headline)
+                .fontWeight(.medium)
+                .multilineTextAlignment(.center)
+                .foregroundColor(Color.MyTheme.purpleColor)
+                .padding()
+            
+            Button(action: {
+                
+            }, label: {
+                SignInWithAppleButtonCustom()
+                    .frame(height: 60)
+                    .frame(maxWidth: .infinity)
+            })
+
+            Button(action: {
+                
+            }, label: {
+                HStack {
+                    Image(systemName: "globe")
+                    
+                    Text("Sign in with Google")
+                }
+                .frame(height: 60)
+                .frame(maxWidth: .infinity)
+                .background(Color(.sRGB, red: 222/255, green: 82/255, blue: 70/255, opacity: 1.0))
+                .cornerRadius(4)
+                .font(.system(size: 23, weight: .medium))
+            })
+            .accentColor(Color.white)
+        }
+        .padding(.all, 20)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.MyTheme.beigeColor)
+        .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct OnboardingView_Previews: PreviewProvider {
+    static var previews: some View {
+        OnboardingView()
+    }
+}
---
+++ DogGram/Views/Subviews/SignInWithAppleButtonCustom.swift
@@ -0,0 +1,20 @@
+//
+//  SignInWithAppleButtonCustom.swift
+//  DogGram
+//
+//  Created by 大和田一裕 on 2022/06/29.
+//
+
+import Foundation
+import SwiftUI
+import AuthenticationServices
+
+struct SignInWithAppleButtonCustom: UIViewRepresentable {
+    
+    func makeUIView(context: Context) -> ASAuthorizationAppleIDButton {
+        return ASAuthorizationAppleIDButton(authorizationButtonType: .default, authorizationButtonStyle: .black)
+    }
+    
+    func updateUIView(_ uiView: ASAuthorizationAppleIDButton, context: Context) {}
+    
+}
